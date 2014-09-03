3f2ae985fe442baea42eea05039fd25d760344b6
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-02 19:36:34 -0600

    Kitten create

--- app/controllers/kitten_categories_controller.rb
+++ app/controllers/kitten_categories_controller.rb
@@ -3,4 +3,14 @@ class KittenCategoriesController < ApplicationController
   def new
    @kitten_category = KittenCategory.new
   end
+
+  def create
+    @kitten_category = KittenCategory.new(
+      category: params[:kitten_category][:category])
+
+    if @kitten_category.save
+      flash[:error] = "Kitten Category has already been taken"
+      redirect_to root_path
+    end
+  end
 end
